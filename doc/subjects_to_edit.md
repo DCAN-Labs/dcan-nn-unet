77ec2e58fa932cd57b7e89d5213f7b61f392ea79
paul-reiners <paul.reiners@gmail.com>
2021-08-09 09:10:23 -0500

    Adding images.

---
+++ doc/subjects_to_edit.md
@@ -0,0 +1,33 @@
+# subjects_to_edit
+
+The nnU-Net inferred segmentations are available here:
+
+    * /home/feczk001/shared/data/nnUNet/segmentations/inferred/subjects_to_edit/
+
+## Images
+
+Here we have the images layered (from top to bottom):
+
+1. Segmentation (50--100% opacity)
+2. T1 (50% opacity)
+3. T2
+
+I also added smoothing.
+
+### 2 months
+
+#### sub-684448
+
+Predicted sagittal       |  Predicted coronal | Predicted axial
+:-------------------------:|:-------------------------:|:-------------------------:
+![](../img/subjects_to_edit/2mo/sub-684448/sagittal.jpeg)  |  ![](../img/subjects_to_edit/2mo/sub-684448/coronal.jpeg)  |  ![](../img/subjects_to_edit/2mo/sub-684448/axial.jpeg)
+![](../img/subjects_to_edit/2mo/sub-684448/sagittal-outline.jpeg)  |  ![](../img/subjects_to_edit/2mo/sub-684448/coronal-outline.jpeg)  |  ![](../img/subjects_to_edit/2mo/sub-684448/axial-outline.jpeg)
+
+### 3 months
+
+#### sub-116056
+
+Predicted sagittal       |  Predicted coronal | Predicted axial
+:-------------------------:|:-------------------------:|:-------------------------:
+![](../img/subjects_to_edit/3mo/sub-116056/sagittal.jpeg)  |  ![](../img/subjects_to_edit/3mo/sub-116056/coronal.jpeg)  |  ![](../img/subjects_to_edit/3mo/sub-116056/axial.jpeg)
+![](../img/subjects_to_edit/3mo/sub-116056/sagittal-outline.jpeg)  |  ![](../img/subjects_to_edit/3mo/sub-116056/coronal-outline.jpeg)  |  ![](../img/subjects_to_edit/3mo/sub-116056/axial-outline.jpeg)
--- nnunet/dataset_conversion/move_baby_files_around.py
+++ nnunet/dataset_conversion/move_baby_files_around.py
@@ -1,22 +1,25 @@
 import os
 from shutil import copyfile
+import sys
 
 
-origin_dir = '/home/miran045/reine097/JLF_templates_testing/wm_JLF_atlases'
-destination_dir = '/home/miran045/reine097/JLF_templates_testing/flat_wm_JLF_atlases/'
+def main(origin_folder, destination_folder):
+    for sub_folder in os.listdir(origin_folder):
+        for template_folder in os.listdir(origin_folder + '/' + sub_folder):
+            if template_folder == 'T2w_files':
+                continue
+            current_folder = os.path.join(origin_folder, sub_folder, template_folder)
+            t1_file = os.path.join(current_folder, 'T1w_acpc_dc_restore.nii.gz')
+            t1_destination_file = \
+                "{}/images/{}_{}_0000.nii.gz".format(destination_folder, sub_folder, template_folder)
+            copyfile(t1_file, t1_destination_file)
+            t2_file = os.path.join(origin_folder, sub_folder, 'T2w_files', template_folder + '_T2w_acpc_dc_restore.nii.gz')
+            t2_destination_file = \
+                "{}/images/{}_{}_0001.nii.gz".format(destination_folder, sub_folder, template_folder)
+            copyfile(t2_file, t2_destination_file)
 
-for sub_folder in os.listdir(origin_dir):
-    for template_folder in os.listdir(origin_dir + '/' + sub_folder):
-        current_folder = origin_dir + '/' + sub_folder + '/' + template_folder
-        segmentation_file = current_folder + '/Segmentation.nii.gz'
-        destination_segmentation_file = \
-            "{}/labels/{}_{}.nii.gz".format(destination_dir, sub_folder, template_folder)
-        copyfile(segmentation_file, destination_segmentation_file)
-        t1_file = current_folder + '/T1w_brain.nii.gz'
-        t1_destination_file = \
-            "{}/images/{}_{}_0000.nii.gz".format(destination_dir, sub_folder, template_folder)
-        copyfile(t1_file, t1_destination_file)
-        t2_file = current_folder + '/T2w_brain.nii.gz'
-        t2_destination_file = \
-            "{}/images/{}_{}_0001.nii.gz".format(destination_dir, sub_folder, template_folder)
-        copyfile(t2_file, t2_destination_file)
+
+if __name__ == "__main__":
+    origin_dir = sys.argv[1]
+    destination_dir = sys.argv[2]
+    main(origin_dir, destination_dir)
