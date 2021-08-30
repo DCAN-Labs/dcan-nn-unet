316894e363e4e84def8015f243901fe32d5a00a2
paul-reiners <paul.reiners@gmail.com>
2021-08-30 09:10:35 -0500

    Generalized removal of substring function.

--- nnunet/dataset_conversion/remove_substring_from_files.py
+++ nnunet/dataset_conversion/remove_substring_from_files.py
@@ -4,14 +4,12 @@ from os import listdir
 from os.path import isfile, join
 
 
-def main(fldr):
+def main(fldr, substring):
     onlyfiles = [f for f in listdir(fldr) if isfile(join(fldr, f))]
     for file_name in onlyfiles:
         new_file_name = file_name
-        if '_T1w' in file_name:
-            new_file_name = file_name.replace('_T1w', '')
-        if '_T2w' in file_name:
-            new_file_name = file_name.replace('_T2w', '')
+        if substring in file_name:
+            new_file_name = file_name.replace(substring, '')
         file = join(fldr, file_name)
         destination = join(fldr, new_file_name)
         os.rename(file, destination)
@@ -19,4 +17,4 @@ def main(fldr):
 
 if __name__ == "__main__":
     folder = sys.argv[1]
-    main(folder)
+    main(folder, sys.argv[2])
