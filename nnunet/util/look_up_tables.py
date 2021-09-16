7c6b2e31aea03ea6f4a255e0dad1c717c0646c31
paul-reiners <paul.reiners@gmail.com>
2021-09-16 12:08:07 -0500

    Added code to correct chirality.

---
+++ nnunet/util/look_up_tables.py
@@ -0,0 +1,17 @@
+def get_id_to_region_mapping(mapping_file_name, separator=None):
+    file = open(mapping_file_name, 'r')
+    lines = file.readlines()
+
+    id_to_region = {}
+    for line in lines:
+        line = line.strip()
+        if line.startswith('#') or line == '':
+            continue
+        if separator:
+            parts = line.split(separator)
+        else:
+            parts = line.split()
+        region_id = int(parts[0])
+        region = parts[1]
+        id_to_region[region_id] = region
+    return id_to_region
