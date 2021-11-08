807f4c6ecb6190867e20de5a44c2a2f3d26dc711
paul-reiners <paul.reiners@gmail.com>
2021-11-08 09:52:13 -0600

    Rename image files, both T1 and T2.

--- dcan/dataset_conversion/rename_image_files.py
+++ dcan/dataset_conversion/rename_image_files.py
@@ -11,7 +11,13 @@ def main(image_dir):
         image_file = image_files[i]
         # Get rid of ".nii.gz" extension
         image_file_base = image_file[:-7]
-        new_image_file_name = image_file_base + "_0000.nii.gz"
+        if 'T1w_' in image_file_base:
+            new_image_file_base = image_file_base.replace('T1w_', '')
+            new_image_file_name = new_image_file_base + "_0000.nii.gz"
+        else:
+            new_image_file_base = image_file_base.replace('T2w_', '')
+            new_image_file_name = new_image_file_base + "_0001.nii.gz"
+
         os.rename(image_dir + '/' + image_file, image_dir + '/' + new_image_file_name)
 
 
