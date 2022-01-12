0e0ab64721c76e46a606f99b4e0e0e0ec437a7cb
paul-reiners <paul.reiners@gmail.com>
2022-01-12 17:01:32 -0600

    Template basically working now.

--- dcan/evaluation/calculate_dice_for_folder.py
+++ dcan/evaluation/calculate_dice_for_folder.py
@@ -1,30 +1,36 @@
 """Usage: calculate_dice_for_folder <ground_truth_folder> <inferred_folder>"""
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
 
 from docopt import docopt
 
-from calculate_dice import calculate_dice_coefficient
-
 
 # Author: Paul Reiners
+from dcan.evaluation.calculate_dice import calculate_dice_coefficient
 
 
 def calculate_dice_for_folder(gt_folder, inferred_fldr):
+    if not exists(gt_folder) or not exists(inferred_fldr):
+        return {}
     inferred_files = [f for f in listdir(inferred_fldr) if isfile(join(inferred_fldr, f))]
     file_count = 0
     total = 0.0
+    results = {}
+    file_results = {}
     for inferred_file in inferred_files:
         if inferred_file.endswith('pkl') or inferred_file.endswith('json'):
             continue
         file_count += 1
         inferred_file_path = join(inferred_fldr, inferred_file)
-        print("inferred_file:", inferred_file)
         gt_file_path = join(gt_folder, inferred_file)
         dice_coefficient = calculate_dice_coefficient(gt_file_path, inferred_file_path)
         total += dice_coefficient
-        print('dice (foreground):', dice_coefficient)
-    print('\navg dice:', total / file_count)
+        file_results[inferred_file] = dice_coefficient
+    results['file_results'] = file_results
+    avg_dice = total / file_count
+    results['avg_dice'] = avg_dice
+
+    return results
 
 
 if __name__ == "__main__":
--- dcan/paper/generate_cross_validation_report.py
+++ dcan/paper/generate_cross_validation_report.py
@@ -1,4 +1,9 @@
+import os.path
+
 from jinja2 import Environment, PackageLoader, select_autoescape
+
+from dcan.evaluation.calculate_dice_for_folder import calculate_dice_for_folder
+
 env = Environment(
     loader=PackageLoader("dcan.paper"),
     autoescape=select_autoescape()
@@ -6,4 +11,14 @@ env = Environment(
 
 template = env.get_template("10FoldValidationResults.md")
 
-print(template.render(the="variables", go="here"))
+data = {}
+segmentations_root_folder = '/home/feczk001/shared/data/nnUNet/segmentations/inferred/PaperCrossValidation/'
+segmentations_folders = ['Task{}_Paper_Fold{}'.format(516 + i, i) for i in range(10)]
+gt_folder = '/home/feczk001/shared/data/nnUNet/raw_data/Task516_525/'
+for segmentation_folder in segmentations_folders:
+    inferred_fldr = os.path.join(segmentations_root_folder, segmentation_folder)
+    dice_scores = calculate_dice_for_folder(gt_folder, inferred_fldr)
+    if dice_scores:
+        data[inferred_fldr] = dice_scores
+
+print(template.render(dict_item=data))
