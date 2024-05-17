8aba067e12eaeefe9c1f4bc821c2d2fe823213a2
Paul Reiners <reine097@umn.edu>
2024-05-17 09:54:39 -0500

    Finished DICE for 527.

--- dcan/paper/load_dice_data.py
+++ dcan/paper/load_dice_data.py
@@ -1,12 +1,24 @@
 import pickle
+import sys
+import csv
+
+from util.look_up_tables import get_id_to_region_mapping
 
 
-def loadData():
-    # for reading also binary mode is important
-    dbfile = open('../../data/dice.pickle', 'rb')
+def load_data(data_location, csv_location):
+    dbfile = open(data_location, 'rb')
+    free_surfer_color_lut = '/home/miran045/reine097/projects/abcd-nn-unet/look_up_tables' \
+                            '/Freesurfer_LUT_DCAN.txt'
     db = pickle.load(dbfile)
-    for keys in db:
-        print(keys, '=>', db[keys])
+    free_surfer_label_to_region = get_id_to_region_mapping(free_surfer_color_lut)
+    with open(csv_location, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['reference', 'segment_name', 'Dice'])
+        for keys in db:
+            for dice_score in db[keys]:
+                writer.writerow([keys, free_surfer_label_to_region[keys], dice_score])
     dbfile.close()
 
-loadData()
+
+if __name__ == "__main__":
+    load_data(sys.argv[1], sys.argv[2])
