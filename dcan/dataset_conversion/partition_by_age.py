23f29e54d7c53fa98feec4bd41da4b13b320ad01
Paul Reiners <reine097@umn.edu>
2024-05-22 16:21:13 -0500

    Generalized source directory to command-line arg.

--- dcan/dataset_conversion/partition_by_age.py
+++ dcan/dataset_conversion/partition_by_age.py
@@ -1,16 +1,21 @@
 import os.path
+import sys
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
 import shutil
 
-src_dir = '/home/feczk001/shared/data/nnUNet/528/labelsTs'
-only_files = [f for f in listdir(src_dir) if isfile(join(src_dir, f))]
+def main(src_dir):
+    only_files = [f for f in listdir(src_dir) if isfile(join(src_dir, f))]
 
-for month in range(9):
-    Path(os.path.join(src_dir, f'{month}mo')).mkdir(parents=True, exist_ok=True)
+    for month in range(9):
+        Path(os.path.join(src_dir, f'{month}mo')).mkdir(parents=True, exist_ok=True)
 
-for f in only_files:
-    mo = f[0]
-    dest_dir = os.path.join(src_dir, f'{mo}mo')
-    shutil.copy(join(src_dir, f), dest_dir)
+    for f in only_files:
+        mo = f[0]
+        dest_dir = os.path.join(src_dir, f'{mo}mo')
+        shutil.move(join(src_dir, f), dest_dir)
+
+if __name__ == '__main__':
+    src_dir = sys.argv[1]
+    main(src_dir)
